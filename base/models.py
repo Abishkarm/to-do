6ad290b2abc7975281f8d87c2ec0885c81bd57d9
Abishkaram <abishkarshrestha@gmail.com>
2024-04-27 16:51:45 +0545

    todo1

--- base/models.py
+++ base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class student(models.Model):
+class todo(models.Model):
     name=models.CharField(max_length=100)
-    status=models.TextField()
-    description=models.CharField(max_length=200)
+    description=models.TextField()
+    status=models.CharField(max_length=200)
--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
+from importlib.resources import contents
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import todo
 # Create your views here.
+def home(request):
+    todos=todo.objects.all()
+    content = {'todos':todos}
+    return render(request,'index.html', context=content)
+
